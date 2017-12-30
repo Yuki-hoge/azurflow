1389af30ca42859c63407a552c572f0017dafd72
ya0201 <you@example.com>
2017-12-30 09:59:54 +0900

    Fix a little in codes using trained model code

--- azurflow.py
+++ azurflow.py
@@ -8,8 +8,8 @@ model = train.create_model(keep_prob=1)
 model.load_weights('model_weights.h5')
 
 img_array = au.load_image('memories/memory1/0.png')
-x = np.array((img_array, img_array))
-# predicted_y = model.predict(x, batch_size=1, verbose=0) * 100
+x = np.array((img_array,))
 predicted_y = model.predict(x, batch_size=1, verbose=0) * 100
 
-print (predicted_y)
+print type(predicted_y[0][0])
+print (predicted_y[0])
